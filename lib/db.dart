612e7cf9e6b644618638eae8cfb10aeff6265386
paulyanaa <paulyana2000@gmail.com>
2025-08-20 22:01:21 -0300

    bd online

--- lib/db.dart
+++ lib/db.dart
@@ -11,11 +11,11 @@ class Db {
   Future<PostgreSQLConnection> get connection async {
     if (_conn == null || _conn!.isClosed) {
       final env = Platform.environment;
-      final host = env['DB_HOST'] ?? 'localhost';
-      final port = 5050;
-      final db   = env['DB_NAME'] ?? 'minhaEstante';
-      final user = env['DB_USER'] ?? 'postgres';
-      final pass = env['DB_PASSWORD'] ?? 'postgres';
+      final host = env['DB_HOST'] ?? 'aws-1-us-east-2.pooler.supabase.com';
+      final port = 6543;
+      final db   = env['DB_NAME'] ?? 'postgres';
+      final user = env['DB_USER'] ?? 'postgres.zyuffpdceibvhkkrhnhb';
+      final pass = env['DB_PASSWORD'] ?? 'Pauly20122000!';
 
       _conn = PostgreSQLConnection(host, port, db, username: user, password: pass);
       await _conn!.open();
